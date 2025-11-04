98c522414442d5de8450c4366b64f4ac9b1d94f9
Thomas Frohwein <tfroh@live.com>
2025-11-04 14:25:36 -0600

    Fixed to dist

--- backend/app.js
+++ backend/app.js
@@ -19,12 +19,12 @@ app.use('/api/login', loginRouter)
 app.use('/api/odds', oddsRouter)
 app.use('/api/bets', betRouter)
 
-// Serve React build in production
+// Serve React dist in production
 if (config.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'frontend/build')))
+    app.use(express.static(path.join(__dirname, 'frontend/dist')))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'))
+        res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'))
     })
 }
 
