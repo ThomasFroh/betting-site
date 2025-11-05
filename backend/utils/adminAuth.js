bed9af380592679208adb38b6048c660954ed446
Thomas Frohwein <tfroh@live.com>
2025-11-05 15:08:13 -0600

    Removed print

--- backend/utils/adminAuth.js
+++ backend/utils/adminAuth.js
@@ -66,8 +66,6 @@ const requireAdmin = (req, res, next) => {
 const requireAuth = (req, res, next) => {
   try {
     const authHeader = req.headers.authorization
-
-    console.log('authHeader', authHeader)
     
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
       return res.status(401).json({ 
